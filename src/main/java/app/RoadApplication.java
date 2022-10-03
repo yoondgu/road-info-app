186c71564b5225fbb5b946e80a7dbbd56286c6b8
yoondgu <doityoungg@gmail.com>
2022-10-03 20:37:39 +0900

    fix comments

--- src/main/java/app/RoadApplication.java
+++ src/main/java/app/RoadApplication.java
@@ -10,8 +10,9 @@ public class RoadApplication {
 
 	public static void main(String[] args) {
 		// TODO AWS Lambda 통해 값 반환  (나중에)
-		// TODO 경로를 통해 도로 정보 획득하는 것도 따로 해보기
-		ResponseData responseData = RoadService.getRoadInfo(0.015); // km단위로 단위거리 입력(ex: 0.05 = 최소 50m 간격의 좌표들을 기준으로 조회)
+		// TODO 요청한 API에서 좌표 선택 기준, idxName 등 정보 문의해서 도로정보 자세히 얻기
+		// TODO 경로검색 등 다른 방식으로 도로정보 획득해보기
+		ResponseData responseData = RoadService.getRoadInfo(0.02); // km단위로 단위거리 입력(ex: 0.05 = 최소 50m 간격의 좌표들을 기준으로 조회)
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String response = gson.toJson(responseData);
 		System.out.println(response);
--- src/main/java/vo/Coord.java
+++ src/main/java/vo/Coord.java
@@ -2,14 +2,14 @@ package vo;
 
 public class Coord {
 
-	private int csvIdx; // csv파일의 행순서 (0번부터 저장, csvIdx가 0이면 csv파일의 2번째 행이다.)
 	private double latitude;
 	private double longitude;
+	private int csvIdx; // csv파일의 행순서 (0번부터 저장, csvIdx가 0이면 csv파일의 2번째 행이다.)
 	
 	public Coord(double latitude, double longitude, int csvIdx) {
-		this.csvIdx = csvIdx;
 		this.latitude = latitude;
 		this.longitude = longitude;
+		this.csvIdx = csvIdx;
 	}
 	
 	public int getCsvIdx() {
